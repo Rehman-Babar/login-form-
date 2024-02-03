70fc9560830ee82631d0a7954da1a1d2882665e5
Rehman-Babar327 <153273576+Rehman-Babar@users.noreply.github.com>
2024-02-03 17:25:49 +0500

    Delete src directory

--- src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import {signup, getUser } from "../controller/user.controller.js";
-const router =Router()
-
-router.post('/signup', signup)
-router.get('/users', getUser)
-// router.post('/login', login)
-
-export default router;
